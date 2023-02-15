7537bf47f5b79d529faeb0064df3131a0e4bd0dc
flying_8lack <megacryleon@gmail.com>
2023-02-16 01:42:54 +0300

    added basic player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import logging
 import util.system
+import util.player
 
 log = logging.Logger(__name__)
 running = True
@@ -8,17 +9,24 @@ pygame.init()
 screen = util.system.Screen()
 bgmusic = util.system.Music()
 Map = util.system.Map("")
-player = util.player
+player = util.player.Player()
 
 log.debug("start loading the map")
 Map.load_map()
 log.debug("Map loading is done")
 
-screen.draw(Map.surface, pygame.Rect(0,0,600,400))
-screen.update()
+
 while running:
+    screen.draw(Map.surface, pygame.Rect(0, 0, 600, 400))
+    screen.draw(player.image, player.rect)
+    screen.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             screen.quit()
             bgmusic.quit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.dict["key"] == pygame.K_d:
+
+                player.move(1, 0)
--- util/player.py
+++ util/player.py
@@ -4,10 +4,20 @@ import pygame.sprite
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
-        super().__init__(self)
+        super().__init__(pygame.sprite.Group())
         self.x = 12
         self.y = 13
-        self.image = pygame.surface.Surface([16, 16])#pygame.image.load("data\\texture\\player.png")
+        self.image = pygame.surface.Surface([16, 16])  # pygame.image.load("data\\texture\\player.png")
         self.image.fill((255, 0, 255))
-        self.rect = pygame.Rect(self.x, self.y, 16, 16)
+        self.rect = pygame.Rect(self.x*16, self.y*16, 16, 16)
 
+    def update_rect(self):
+        self.rect = pygame.Rect(self.x*16, self.y*16, 16, 16)
+    def moveTo(self, x, y):
+        self.x, self.y = x, y
+        self.update_rect()
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+        self.update_rect()
