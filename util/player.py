161b18878ce7ce34639a15024a6ae1d9c31d03fb
flying_8lack <megacryleon@gmail.com>
2023-02-17 21:53:35 +0300

    rebased inventory onto master

--- util/player.py
+++ util/player.py
@@ -3,6 +3,8 @@ import math
 
 import pygame.sprite
 
+import GUI.Inventory
+
 import util.system
 
 
@@ -20,6 +22,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.surface.Surface([16, 16])  # pygame.image.load("data\\texture\\player.png")
         self.image.fill((255, 0, 255))
         self.rect = pygame.Rect(self.x * 16, self.y * 16, 16, 16)
+
+        self.inventory = GUI.Inventory.Inventory()
 
     def update_rect(self):
         self.rect = pygame.Rect(self.x * 16, self.y * 16, 16, 16)
