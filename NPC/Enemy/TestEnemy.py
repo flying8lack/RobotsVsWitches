668d4db7c33e82ec135d140dba9c95f9e2dc2017
flying_8lack <megacryleon@gmail.com>
2023-02-22 15:20:08 +0300

    added test enemy

---
+++ NPC/Enemy/TestEnemy.py
@@ -0,0 +1,29 @@
+import pygame
+
+from NPC.Enemy.AbstractEnemy import AbstractEnemy
+from battle.Stats import Stat
+
+
+class TestEnemy(AbstractEnemy):
+
+    def __init__(self):
+        self.surface = pygame.Surface([16, 16])
+        self.surface.fill((240, 98, 20))
+
+    def get_stat(self) -> Stat:
+        return Stat(12)
+
+    def get_name(self) -> str:
+        return "TEST"
+
+    def get_surface(self):
+        return self.surface
+
+    def get_battle_surface(self):
+        return self.surface
+
+    def get_battle_rect(self):
+        return self.surface.get_rect()
+
+    def get_rect(self):
+        return self.surface.get_rect()
